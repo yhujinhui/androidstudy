1a9e53b274e767bb08d91cb1daad22ad211ceb93
yhujinhui <52348335+yhujinhui@users.noreply.github.com>
2020-03-21 22:23:38 +0900

    java_03_21

---
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hello;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "버튼이 눌렸어요",Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+    public void onButton3Clicked(View V){
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("tel:010-1000-1000"));
+        startActivity(intent);
+    }
+}
